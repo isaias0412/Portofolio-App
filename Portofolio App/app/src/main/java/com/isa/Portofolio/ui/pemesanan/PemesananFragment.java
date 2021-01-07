2d7ed0387116fbc4347e95af547c67096a2ff7a9
isaias0412 <64055159+isaias0412@users.noreply.github.com>
2021-01-07 21:22:16 +0700

    Add files via upload

---
+++ Portofolio App/app/src/main/java/com/isa/Portofolio/ui/pemesanan/PemesananFragment.java
@@ -0,0 +1,4 @@
+package com.isa.Portofolio.ui.pemesanan;
+
+public class PemesananFragment {
+}
---
+++ Portofolio App/app/src/main/java/com/isa/Portofolio/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,18 @@
+package com.isa.Portofolio.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
